23dfc96657027921c25d2d54a4c2060636c909d7
leolleocomp <leolleo.comp@gmail.com>
2018-12-09 13:38:50 -0300

    Add favico e Rm imagem de background do drawer

--- frontend/src/components/core/Drawer.vue
+++ frontend/src/components/core/Drawer.vue
@@ -9,52 +9,47 @@
     mobile-break-point="991"
     width="260"
   >
-    <v-img
-      :src="image"
-      height="100%"
+    <v-layout
+      class="fill-height"
+      tag="v-list"
+      column
     >
-      <v-layout
-        class="fill-height"
-        tag="v-list"
-        column
-      >
-        <v-list-tile avatar>
-          <v-list-tile-avatar
-            color="white"
-          >
-            <v-avatar
-              height="34"
-              contain
-            >
-              <img
-                :src="logo"
-                alt="PIPE flow"
-              >
-            </v-avatar>
-
-          </v-list-tile-avatar>
-          <v-list-tile-title class="title">
-            PIPE Flow
-          </v-list-tile-title>
-        </v-list-tile>
-        <v-divider/>
-        <v-list-tile
-          v-for="(link, i) in links"
-          :key="i"
-          :to="link.to"
-          :active-class="color"
-          avatar
-          class="v-list-item"
+      <v-list-tile avatar>
+        <v-list-tile-avatar
+          color="white"
         >
-          <v-list-tile-action>
-            <v-icon>{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title
-            v-text="link.text"
-          />
-        </v-list-tile>
-      </v-layout>
-    </v-img>
+          <v-avatar
+            height="34"
+            contain
+          >
+            <img
+              :src="logo"
+              alt="PIPE flow"
+            >
+          </v-avatar>
+
+        </v-list-tile-avatar>
+        <v-list-tile-title class="title">
+          PIPE Flow
+        </v-list-tile-title>
+      </v-list-tile>
+      <v-divider/>
+      <v-list-tile
+        v-for="(link, i) in links"
+        :key="i"
+        :to="link.to"
+        :active-class="color"
+        avatar
+        class="v-list-item"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title
+          v-text="link.text"
+        />
+      </v-list-tile>
+    </v-layout>
   </v-navigation-drawer>
 </template>
 
