03b52fa8dec004016db33eff93131b801d8d9537
leolleocomp <leolleo.comp@gmail.com>
2018-12-09 14:37:58 -0300

    Add horário da última leitura

--- frontend/src/components/app/SensorCard.vue
+++ frontend/src/components/app/SensorCard.vue
@@ -48,7 +48,7 @@
       </v-layout>
 
       <v-layout
-        v-else
+        v-if="flow === null"
         row
         justify-space-around
         class="pl-3 pr-3"
@@ -56,7 +56,15 @@
         <v-progress-linear indeterminate />
       </v-layout>
     </v-card-text>
-    <v-card-actions class="pb-5" />
+    <v-card-actions class="pb-5">
+      <span
+        v-if="lastTimestamp"
+        :class="irregular ? 'red--text' : 'grey--text'"
+      >
+        Última leitura às {{ lastTimestamp }}
+      </span>
+      <span v-else />
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -110,6 +118,10 @@ export default {
 
     flow () {
       return this.readings.length ? _.last(this.readings).flow : null
+    },
+
+    lastTimestamp () {
+      return this.readings.length ? _.last(this.readings).timestamp : null
     }
   }
 }
